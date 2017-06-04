a2cd3fb2bf7ecbece3bf6b2113494084f2acc7a2
Ewart Jackson <ewartjackson@icloud.com>
2017-06-04 09:02:13 -0700

    build class Movie and instance for wonder woman

---
+++ entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+wonder_woman = media.Movie("Wonder Woman",
+                           "Before she was Wonder Woman (Gal Gadot), she was Diana, princess of the Amazons, trained to be an unconquerable warrior. Raised on a sheltered island paradise, Diana meets an American pilot (Chris Pine) who tells her about the massive conflict that's raging in the outside world. Convinced that she can stop the threat, Diana leaves her home for the first time. Fighting alongside men in a war to end all wars, she finally discovers her full powers and true destiny.",
+                           "https://upload.wikimedia.org/wikipedia/en/e/ed/Wonder_Woman_%282017_film%29.jpg",
+                           "https://www.youtube.com/watch?v=F0lZgaz0CIE")
+print(wonder_woman.storyline)
